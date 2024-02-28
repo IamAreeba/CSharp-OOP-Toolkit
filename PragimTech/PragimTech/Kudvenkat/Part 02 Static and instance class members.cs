5342c3872062bebdfed646fe8e0001ad7d5acb1b
Areeba Amjad <areeba.amjad2020@gmail.com>
2024-02-28 00:15:09 -0800

    Commit 02 (KOOP) : Static and instance class members

---
+++ PragimTech/PragimTech/Kudvenkat/Part 02 Static and instance class members.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Circle
+{
+    //float _PI = 3.141F;
+    static float _PI;
+    public static string _staticField;
+    int _Radius;
+
+    // Static Constructor
+    static Circle()
+    {
+        Console.WriteLine("Static Constructor Called \n");
+        Circle._PI = 3.141f;
+        Circle._staticField = "Firstly i have made myseld public so in main i have been directly accessed but i am init the static constructor so when u are aceesing me i am init before u have called me ";
+    }
+
+    // Instance Constructor
+    public Circle(int Radius)
+    {
+        Console.WriteLine("\nInstance Constructor Called");
+        this._Radius = Radius;
+    }
+
+    public float CalculateArea()
+    {
+        //return this._PI * this._Radius * this._Radius;
+        return Circle._PI * this._Radius * this._Radius;
+    }
+
+    public static void Print()
+    {
+        Console.WriteLine("\n I am static method. And i have 1 field static which is _PI = {0}", Circle._PI);
+    }
+
+}
+
+
+class Program
+{
+    public static void Main()
+    {
+
+        Console.WriteLine(Circle._staticField);
+
+        Circle C1 = new Circle(5);
+        float Area1 = C1.CalculateArea();
+        Console.WriteLine("Area1 = {0}", Area1);
+
+        Circle C2 = new Circle(6);
+        float Area2 = C2.CalculateArea();
+        Console.WriteLine("Area2 = {0}", Area2);
+
+        Circle.Print();
+
+        Console.ReadLine();
+    }
+}
--- PragimTech/PragimTech/Notes/Notes.cs
+++ PragimTech/PragimTech/Notes/Notes.cs
@@ -11,3 +11,19 @@
  * 
  */
 
+// ===================================  02 =============================================
+/*
+ * Something which is not changing we make them static
+ * All class members(fields, constructors etc.) can be either static or instance
+ * 
+ * To init instance field use instance constructor.
+ * To init static field use static constructor.
+ * AM are not allowed on static constructor
+ * We dont have to call static constructor explicitly. Cuz when we are refering static field Constructor will be automatically called before that to init static field. Why?
+ * Cuz if u make it public someone will call that constructor
+ * 
+ * Static Constructors are called before instace constructor and even before u refer to any static field cuz the need to be init and called only once to init the obj but instace will be called every time whenever u create the obj
+ * 
+ */
+
+
